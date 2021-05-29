209e420de33d1a904d1e35fa856346e00fcdb371
Vandivier <john@afterecon.com>
2021-05-29 13:48:18 -0400

    feat: user schema

---
+++ prisma/migrations/20210529174740_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "first_name" TEXT,
+    "fk_auth_id" UUID NOT NULL,
+    "last_name" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "username" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
